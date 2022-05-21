9b12becd9a3fda4ef11f01aff3fbcc703d44e5be
IamTheo2000 <93789269+IamTheo2000@users.noreply.github.com>
2022-05-22 00:19:43 +0300

    Add files via upload

---
+++ Data/scientific_field.sql
@@ -0,0 +1,5 @@
+insert into scientific_field (field_id, name) values (1, 'Biology');
+insert into scientific_field (field_id, name) values (2, 'Physics');
+insert into scientific_field (field_id, name) values (3, 'Mathematics');
+insert into scientific_field (field_id, name) values (4, 'Chemistry');
+insert into scientific_field (field_id, name) values (5, 'Technology');
